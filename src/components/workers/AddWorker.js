a68fc472abb1caaa7cef349c43eaf32a4b710c88
Nishalya Pethiyagoda <100584754+NishalyaPethiyagoda@users.noreply.github.com>
2023-03-15 17:31:19 +0530

    add worker done

--- src/components/workers/AddWorker.js
+++ src/components/workers/AddWorker.js
@@ -4,9 +4,13 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { InputLabel, MenuItem, TextField } from '@mui/material';
-import { Form } from 'react-router-dom';
-import { Margin } from '@mui/icons-material';
+import { MenuItem, TextField } from '@mui/material';
+import  { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Grid } from '@mui/material';
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+// import { LocalizationProvider } from '@mui/lab';
+// import DateTimePicker from '@mui/lab/DateTimePicker';
 
 
 const style = {
@@ -22,26 +26,36 @@ const style = {
   p: 4,
 };
 
-const designations = [
-    {
-        value: 'Worker',
-        label: 'Worker',
-    },
-    {
-        value: 'Captain',
-        label: 'Captain',
-    },
-    {
-        value: 'CEO',
-        label: 'CEO',
-    }
-];
-
 function AddWorker() {
 
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    const [positions, setPositions] = useState([]);
+    const [newWorker, setWorker] = useState(null);
+    
+    const [ postAlert , setpostAlert] = useState(null);
+    
+
+    useEffect(()=>{
+        
+        axios.get('http://localhost:12759/api/WorkerDesignation')
+            .then(response => {
+                setPositions(response.data);
+            })
+    }, []);
+
+    const handleFormSubmit = ()=>{
+        axios.post('http://localhost:12759/api/Worker', newWorker)
+        .then(response => {
+            setpostAlert(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+        handleClose();
+    }
 
     return (
         <div>
@@ -55,37 +69,82 @@ function AddWorker() {
                 open={open}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
+                autocomplete="off"
             >
-            
                 <Box sx={style}>
                     <Typography id="modal-modal-title" variant="h6" component="h2">
                         Add Worker
                     </Typography>
 
-                    <TextField id="outlined-basic" label="Name" sx={{minWidth: 295, marginTop:3}} variant="outlined" />
+                    <TextField 
+                        id="outlined-basic" 
+                        label="Name" 
+                        sx={{minWidth: 295, marginTop:3}} 
+                        variant="outlined"  
+                        onChange={(e) => setWorker({...newWorker, Name: e.target.value})}
+                        autocomplete="off"
+                    />
 
-                    <TextField id="outlined-basic" label="Age" sx={{minWidth: 295, marginTop:3}} variant="outlined" />
+                    <TextField 
+                        id="outlined-basic" 
+                        label="Age" 
+                        sx={{minWidth: 295, marginTop:3}} 
+                        variant="outlined"  
+                        autocomplete="off"
+                        onChange={(e) => setWorker({...newWorker, Age: parseInt(e.target.value)})}
+                    />
 
-                    <TextField id="outlined-basic" label="Email" sx={{minWidth: 295 , marginTop:3}} variant="outlined" />
+                    <TextField 
+                        id="outlined-basic" 
+                        label="Email" 
+                        sx={{minWidth: 295 , marginTop:3}} 
+                        variant="outlined" 
+                        autocomplete="off"
+                        onChange={(e) => setWorker({...newWorker, Email: e.target.value})}
+                    ></TextField>
 
                     <TextField 
                         select 
                         id="outlined-select" 
                         label="Designation" 
                         variant="filled"
-                        defaultValue="Worker"
+                        //defaultValue={positions}
+                        //defaultValue={worker.designation}
+                        onChange={(e) => setWorker({...newWorker, DesignationId: e.target.value})}
+                        autocomplete="off"
                         sx={{minWidth: 295, marginTop:3}}
                     >
-                        {designations.map( (option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                            { option.label}
+                        {positions.map( (position) => (
+                            <MenuItem 
+                                key={position.id} 
+                                value={position.id}
+                            >
+                                { position.name}
                             </MenuItem>
-                        )
-                        )}
+                            ))
+                        }
                     </TextField>
 
-                    <TextField id="outlined-basic" label="Worker Certified Until (Date)" sx={{minWidth: 295, marginTop:3}} variant="outlined" />
-                    <Button variant="contained" onClick={null} sx={{margin: 3, minWidth:100}}>Submit</Button>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="outlined-basic"
+                                label="Worker Certified Until (Date)"
+                                sx={{minWidth: 295, marginTop:3}}
+                                variant="outlined"
+                                type="date"
+                                onChange={(e) => setWorker({ ...newWorker, CertifiedDate: e.target.value })}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+
+                    <Button variant="contained" onClick={handleFormSubmit} sx={{margin: 3, minWidth:100}} autocomplete="off">
+                        Submit
+                        {/* {console.log(newWorker)} */}
+                    </Button>
                     <Button variant="contained" onClick={handleClose} sx={{margin: 3, minWidth:100}}>Close</Button>
                 </Box>
             
