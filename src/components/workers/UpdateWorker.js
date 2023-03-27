73053f4c1734889691278c0b84223dca80f00d80
Nishalya Pethiyagoda <100584754+NishalyaPethiyagoda@users.noreply.github.com>
2023-03-27 16:24:32 +0530

    worker form validations done

--- src/components/workers/UpdateWorker.js
+++ src/components/workers/UpdateWorker.js
@@ -7,6 +7,7 @@ import Modal from '@mui/material/Modal';
 import {  MenuItem, TextField } from '@mui/material';
 import axios from 'axios';
 import { Grid } from '@mui/material';
+import {z} from "zod";
 
 const style = {
     position: 'absolute',
@@ -26,7 +27,23 @@ const style = {
 function UpdateWorker(props ) {
     
     const [positions, setPositions] = useState([]);
-    const [editingWorker, setWorker] = useState(props.worker);
+    const [editingWorker, setWorker] = useState({
+        id: props.worker.id,
+        name: props.worker.name,
+        age: props.worker.age,
+        email: props.worker.email,
+        certifiedDate: new Date(props.worker.certifiedDate),
+        designationId: parseInt(props.worker.designationId),
+    });
+//     const [submittingWorker, setSubmittingWorker] = useState({
+//         id: editingWorker.id,
+//         name: editingWorker.name,
+//         age: editingWorker.age.to,
+//         email: editingWorker.email,
+//  //       certifiedDate: editingWorker.certifiedDate.toISOString(),
+//         certifiedDate: editingWorker.certifiedDate,
+//         designationId: editingWorker.designationId,
+//     });
   
     useEffect(()=>{
         axios.get('http://localhost:12759/api/WorkerDesignation')
@@ -35,21 +52,62 @@ function UpdateWorker(props ) {
             })
     }, []);
 
-    const updateWorkerSubmit = (editingWorker) => {
-        console.log(editingWorker);
+    const validationScheme = z.object( {
+        name: z.string().min(3).max(25),
+        age: z.number().int().positive().min(18).max(60),
+        email: z.string().email().min(5).max(25),
+        //certifiedDate: z.date().min( new Date()),
+        designationId: z.number().min(1)
+    });
 
-        axios.put(`http://localhost:12759/api/Worker/${editingWorker.id}`, editingWorker)
-            .then( response => {
-                console.log(response.data);
-                
-                props.tableRefresh();
-            });
-        props.setOpenUpdateModal(false);
+    const [nameError, setNameError] = useState('');
+    const [ageError, setAgeError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [certifiedDateError, setCertifiedDateError] = useState('');
+    const [designationIdError, setDesignationIdError] = useState('');
+
+    // const handleCertifiedDateTypeConversion = (event) => {
+    //     const certifiedDateChanged = new Date(event.target.value);
+    //     setWorker({...editingWorker, certifiedDate: certifiedDateChanged });
+    // }
+
+    const updateWorkerSubmit = (editingWorker) => {
+
+        const updateResult = validationScheme.safeParse(editingWorker);
+
+        if(!updateResult.success){
+            updateResult.error.format().name? setNameError(updateResult.error.format().name._errors[0]) : setNameError('');
+            updateResult.error.format().age? setAgeError(updateResult.error.format().age._errors[0] ): setAgeError('');
+            updateResult.error.format().email? setEmailError(updateResult.error.format().email._errors[0]) : setEmailError('');
+            //updateResult.error.format().certifiedDate? setCertifiedDateError(updateResult.error.format().certifiedDate._errors[0]) : setCertifiedDateError('');
+            updateResult.error.format().designationId? setDesignationIdError(updateResult.error.format().designationId._errors[0] ): setDesignationIdError('');
+        }
+        else{
+            //setSubmittingWorker(editingWorker);
+
+            axios.put(`http://localhost:12759/api/Worker/${editingWorker.id}`, editingWorker)
+                .then( response => {
+                    console.log(response.data);
+                    
+                    props.tableRefresh();
+                    handleClose();
+                });
+            
+        }
     }
 
     const handleClose = () =>{
+        setNameError('');
+        setAgeError('');
+        setEmailError('');
+        setCertifiedDateError('');
+        setDesignationIdError('');
+
+        setWorker(null);
+
         props.setOpenUpdateModal(false);
     }
+
     return (
         <div>
             <Modal
@@ -64,67 +122,90 @@ function UpdateWorker(props ) {
                     </Typography>
                         
                     <TextField 
+                        error = { nameError!== ''? true: false}
                         id="outlined-basic" 
+                        required={true}
                         label="Name" 
+                        type="text"
+                        sx={{minWidth: 295, marginTop:3}} 
+                        variant="outlined"  
                         defaultValue={editingWorker.name} 
                         onChange={(e) => setWorker({...editingWorker, name: e.target.value})}
-                        sx={{minWidth: 295, marginTop:3}} 
-                        variant="outlined" size='100px'/>
-                          
+                        autocomplete="off"
+                        helperText={nameError}
+                    />
                     <TextField 
+                        error = {ageError!== ''? true: false}
                         id="outlined-basic" 
+                        rewuired={true}
                         label="Age" 
-                        defaultValue={editingWorker.age} 
-                        onChange={(e) => setWorker({...editingWorker, age: e.target.value})}
+                        type="number"
                         sx={{minWidth: 295, marginTop:3}} 
-                        variant="outlined" />
-                        
+                        variant="outlined"  
+                        defaultValue={editingWorker.age} 
+                        onChange={(e) => setWorker({...editingWorker, age: parseInt(e.target.value)})}
+                        autocomplete="off"
+                        helperText={ageError}
+                    />
+                   <TextField 
+                        error= {emailError!==''? true : false}
+                        id="outlined-basic" 
+                        required={true}
+                        label="Email" 
+                        type="email"
+                        sx={{minWidth: 295 , marginTop:3}} 
+                        variant="outlined" 
+                        defaultValue={editingWorker.email}
+                        onChange={(e) => setWorker({...editingWorker, email: e.target.value})}
+                        autocomplete="off"
+                        helperText={emailError}
+                    ></TextField>
+
                     <Grid container spacing={2}>
                         <Grid item xs={6}>
                             <TextField
+                                error = {certifiedDateError!== ''? true: false}
                                 id="outlined-basic"
+                                required ={true}
                                 label="Worker Certified Until (Date)"
                                 sx={{minWidth: 295, marginTop:3}}
                                 variant="outlined"
                                 type="date"
-                                //defaultValue={DateTime.parse(worker.certifiedDate)}
                                 defaultValue={ new Date(editingWorker.certifiedDate).toISOString().slice(0,10)}
-                                onChange={(e) => setWorker({ ...editingWorker, certifiedDate: e.target.value })}
+                                // onChange={handleCertifiedDateTypeConversion}
+                                onChange= {(e) => setWorker({...editingWorker, certifiedDate: e.target.value})}
+                                helperText={certifiedDateError}
                                 InputLabelProps={{
                                     shrink: true,
                                 }}
                             />
                         </Grid>
                     </Grid>
-                        
-                    <TextField 
-                        id="outlined-basic" 
-                        label="Email" sx={{minWidth: 295, marginTop:3}} 
-                        variant="outlined"
-                        defaultValue={editingWorker.email}
-                        onChange={(e) => setWorker({...editingWorker, email: e.target.value})}
-                    />
-
+                    
                     <TextField 
                         select 
+                        error = {designationIdError!==''? true: false}
                         id="outlined-select" 
                         label="Designation" 
+                        required={true}
+                        variant="filled"
                         defaultValue={parseInt(editingWorker.designationId)}
-                        onChange={(e) => setWorker({...editingWorker, designationId: e.target.value})}
-                        
+                        onChange={(e) => setWorker({...editingWorker, designationId: parseInt(e.target.value)})}
+                        autocomplete="off"
+                        helperText={designationIdError}
                         sx={{minWidth: 295, marginTop:3}}
                     >
-                        
-                        {positions.map( (position) => (
+                        {positions.map( (designation) => (
                             <MenuItem 
-                                key={position.id} 
-                                value={position.id}
+                                key={designation.id} 
+                                value={designation.id}
                             >
-                                { position.name}
+                                { designation.name}
                             </MenuItem>
-                            ))
+                            )) 
                         }
                     </TextField>
+                
                     <Button 
                         variant="contained" 
                         onClick={() => updateWorkerSubmit(editingWorker)} 
